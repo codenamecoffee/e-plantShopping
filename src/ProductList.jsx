968cc212adeb801e8cb4b93101cc7adbfa1297c5
codenamecoffee <110579934+codenamecoffee@users.noreply.github.com>
2026-02-19 22:20:16 -0500

    ProductList.jsx enhancements: state management and UI re-rendering issues corrected

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -308,10 +308,10 @@ function ProductList({ onHomeClick }) {
                               <div className="product-description">{plant.description}</div> {/* Display plant description */}
                               <div className="product-cost">${plant.cost}</div> {/* Display plant cost */}
                               <button
-                                className="product-button"
+                                className={ addedToCart[plant.name] ? "product-button added-to-cart" : "product-button"}
                                 onClick={() => handleAddToCart(plant)} // Handle adding plant to cart
                               >
-                                Add to Cart
+                                { addedToCart[plant.name] ? "Added to Cart" : "Add to cart"}
                               </button>
                             </div>
                           ))}
